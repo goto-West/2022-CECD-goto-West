5e8d1d13702a413fdc15dd152425972fd3c848cc
gayoungee_hehe <1503zzz@gmail.com>
2022-07-20 20:23:46 +0900

    feat : Add tensorflow ready function

--- gotoWestApp/App.js
+++ gotoWestApp/App.js
@@ -1,7 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
 
 export default function App() {
+  const [isTfReady,setIsTfReady]=useState(false);
+  useEffect(() => {
+    (async () => {
+      await fn_requestPermisison();
+      await checkTfReady();
+    })()
+  }, [])
+
+  const checkTfReady = async () => {
+    await tf.ready()
+      .then(() => {
+        setIsTfReady(true);
+      })
+      .catch((error) => {
+        console.log("Tensorflow Ready Error", error);
+        return error;
+      });
+  }
+
   return (
     <View style={styles.container}>
       <Text>Hello gotoWest!</Text>
