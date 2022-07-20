4d3f74f37164e30e1d2a95ad8ee9154a4ebe8361
gayoungee_hehe <1503zzz@gmail.com>
2022-07-20 21:11:22 +0900

    feat : Add request camera permission function

--- gotoWestApp/App.js
+++ gotoWestApp/App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { useState, useEffect } from 'react';
-
+import { Camera } from 'expo-camera';
+import { cameraWithTensors } from '@tensorflow/tfjs-react-native/dist/camera/camera_stream';
 import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs-react-native';
 
 export default function App() {
-  const [isTfReady,setIsTfReady]=useState(false);
+  const [isTfReady,setIsTfReady] = useState(false);
+  const [hasPermission,setHasPermission] = useState(null);
   useEffect(() => {
     (async () => {
-      await fn_requestPermisison();
+      await checkPermission();
       await checkTfReady();
     })()
   }, [])
@@ -23,6 +25,11 @@ export default function App() {
         console.log("Tensorflow Ready Error", error);
         return error;
       });
+  }
+
+  const checkPermission = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    setHasPermission(status === 'granted');
   }
 
   return (
